b817de75601687096299b9fd1882bc0180156e02
hritik4651 <hritik4651@gmail.com>
2025-05-22 14:57:37 +0530

    add-test

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World !');
 });
 
+app.get('/test', (req, res) => {
+  res.send('Testing !');
+});
+
 app.listen(5000, () => {
   console.log(`server is running at http://localhost:${5000}`);
 });
