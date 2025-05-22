e03474c48d1e0c886737e58eeeaea8af44cb3940
hritik4651 <hritik4651@gmail.com>
2025-05-22 17:56:37 +0530

    add some code

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World !1');
+  res.send('Hello World !12');
 });
 
 app.get('/test', (req, res) => {
